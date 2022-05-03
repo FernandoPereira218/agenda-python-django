74a6a03ddda2bf0e0aefceed4d4f8c6ed6e0cbbb
FernandoPereira218 <fernandofmp218@gmail.com>
2022-05-03 16:12:39 -0300

    exercicio

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+from core.models import Evento
+
+
+def buscar_evento(request, titulo_evento):
+    teste = Evento.objects.get(titulo=titulo_evento)
+    return HttpResponse('<h1>O local do evento é em {}</h1>'.format(teste.local))
