08eee5483904dea37c33b7d45b41afa6c570ad7b
FernandoPereira218 <fernandofmp218@gmail.com>
2022-05-05 11:26:50 -0300

    mensagens de erro com http404

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 
 # Create your views here.
 from core.models import Evento
@@ -20,7 +22,8 @@ def buscar_evento(request, titulo_evento):
 @login_required(login_url='/login/')
 def listar_eventos(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    evento = Evento.objects.filter(usuario=usuario, data_evento__gt=data_atual)
     dados = {"eventos": evento}
     return render(request, 'agenda.html', dados)
 
@@ -85,7 +88,19 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404
     if usuario == evento.usuario:
         evento.delete()
+    else:
+        raise Http404()
     return redirect('/')
+
+
+@login_required(login_url='/login/')
+def json_listar_evento(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
